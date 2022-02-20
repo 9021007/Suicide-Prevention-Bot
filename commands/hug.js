8f2b56c3dda5d497f5a8e3c0013162e584420544
Gaspard Wierzbinski <gaspard.wierzbinski@gmail.com>
2022-02-20 16:13:35 +0100

    hug command boilerplate

---
+++ commands/hug.js
@@ -0,0 +1,19 @@
+const { Constants, MessageEmbed } = require('discord.js');
+
+module.exports = {
+	command: {
+		name: "hug",
+		description: "Sends a hug to your favorite user",
+		options: [
+			{
+				name: "user",
+				description: "The user to hug",
+				type: Constants.ApplicationCommandOptionTypes.MENTIONABLE,
+			}
+		]
+	},
+
+	default: async (interaction, lang) => {
+		// to add
+	}
+}
