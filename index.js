b8765cbc7b7ac95e68c32ed55e0358f41f099701
Killerjet101 <Stevenson0610@gmail.com>
2023-02-10 20:40:13 -0800

    Remove everything

--- index.js
+++
@@ -1,8 +0,0 @@
-const { ShardingManager } = require('discord.js');
-const { token, totalshards } = require('./config.json');
-
-const manager = new ShardingManager('./bot.js', { token: token, totalShards: totalshards });
-
-manager.on('shardCreate', shard => console.log(`Launched shard ${shard.id + 1}`));
-
-manager.spawn();
