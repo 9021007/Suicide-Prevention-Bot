7a1d7a31bc874e78235a0c60f7e6f21f20371579
Killerjet101 <Stevenson0610@gmail.com>
2023-02-11 17:08:47 -0800

    :D

--- main.py
+++ main.py
@@ -1,23 +1,19 @@
 import json
 import requests
 
-long = input("Enter the name of the language, e.g. German: ")
-
-short = input("Enter the short name of the language, e.g. de: ")
-short = short.lower()
 
 # check for existing englsih file
 try:
     open("en.json", "r")
 except FileNotFoundError:
-    # download english file
-    print("Downloading english file...")
+    # download english file from GitHub
+    print("\033[1;33mDownloading english file...\033[0m")
     r = requests.get("https://raw.githubusercontent.com/Bobrobot1/Suicide-Prevention-Bot/main/locales/en.json")
     
     with open("en.json", "wb") as f:
         f.write(r.content)
     
-    print("Download complete!")
+    print("\033[1;32mDownload complete!\033[0m")
 
 english = open("en.json", "r")
 englishobj = json.load(english)
@@ -25,7 +21,13 @@ englishobj = json.load(english)
 newobj = {}
 print("")
 
-print("You will now be prompted to translate each word in the english file. Press enter to submit.")
+# input the name of the new translation file
+long = input("Enter the name of the language, e.g. German: ")
+short = input("Enter the short name of the language, e.g. de: ")
+short = short.lower()
+
+print("")
+print("\033[1;30mYou will now be prompted to translate each word in the english file. Press enter to submit.\nUse \\n to make a line break.\033[0m")
 print("")
 
 for key in englishobj:
@@ -37,4 +39,5 @@ for key in englishobj:
 
 file = open(short + ".json", "w")
 json.dump(newobj, file, indent=4)
-print("Thank you! The completed translation file has been saved as " + short + ".json")
+print("\033[1;32mThank you! The completed translation file has been saved as " + short + ".json\033[0m")
+print("\033[1;32mGo submit the translation file onto our Discord and notify a staff member to recive your volunteer role!\033[0m")
