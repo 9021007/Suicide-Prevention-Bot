72f2a332288ec276a2003a5fa54ed0bc791a3ce2
9021007 <24487638+9021007@users.noreply.github.com>
2024-07-05 20:04:22 -0700

    Updated Privacy Policy

--- spbot-web/src/routes/privacy/+page.svelte
+++ spbot-web/src/routes/privacy/+page.svelte
@@ -6,84 +6,134 @@
 
 
     <p>
-      Last updated 2024/3/12 <br><br>
-
-
-Thank you for choosing to be part of our community at Suicide Prevention Discord Bot ("Community", "we", "us", "our"). We are committed to protecting your personal information and your right to privacy. If you have any questions or concerns about this privacy notice, or our practices with regards to your personal information, please contact us at our Discord server. <br><br>
-
-When you use our Discord Application, as the case may be (the "bot") and more generally, use any of our services (the "Services", which include the bot), we appreciate that you are trusting us with nothing, because we don't steal your data. We take your privacy very seriously. In this privacy notice, we seek to explain to you in the clearest way possible what information we collect (none), how we use it (we don't) and what rights you have in relation to it. We hope you take some time to read through it carefully, as it is important. If there are any terms in this privacy notice that you do not agree with, please discontinue use of the bot immediately. <br><br>
-
-This privacy notice applies to all information collected through our Services (which, as described above, includes our App), as well as, any related services, sales, marketing or events. <br><br>
-
-Please read this privacy notice carefully as it will help you understand what we do with the information that we collect. <br><br>
-
-<h2 >TABLE OF CONTENTS</h2> <br>
-
+      Last updated 2024/7/5
+    </p>
+    <p>
+      Welcome to the privacy policy! Almost nobody reads this, but I'm glad to see that some people do!
+    </p>
+    <p>
+      Anyway, there's a bit of a checklist when it comes to the requirements of a privacy policy, so here goes.
+    </p>
+    <p>
+      The name of the service is "Suicide Prevention Bot", but I'll be referring to it as "the bot" becuase nobody has time for such a long name.
+    </p>
+    <p>
+      This policy applies to everyone interacting with the bot, or accessing the website. **This means you!**
+    </p>
+    <p>
+      We also have a <a href="/terms">Terms of Service</a> that applies to you. Legally, you're supposed to read that too. Enjoy.
+    </p>
+    <h3>The data we collect</h3>
+    <p>
+      Well to start off, we don't collect any sensitive or personal information, unless you explicitly submit it to us.
+    </p>
 <p>
-1. WHAT INFORMATION DO WE COLLECT? <br>
-2. WILL YOUR INFORMATION BE SHARED WITH ANYONE? <br>
-3. IS YOUR INFORMATION TRANSFERRED INTERNATIONALLY? <br>
-4. HOW LONG DO WE KEEP YOUR INFORMATION? <br>
-5. HOW DO WE KEEP YOUR INFORMATION SAFE? <br>
-6. WHAT ARE YOUR PRIVACY RIGHTS? <br>
-7. CONTROLS FOR DO-NOT-TRACK FEATURES <br>
-8. DO CALIFORNIA RESIDENTS HAVE SPECIFIC PRIVACY RIGHTS? <br>
-9. DO WE MAKE UPDATES TO THIS NOTICE? <br>
-10. HOW CAN YOU CONTACT US ABOUT THIS NOTICE? <br>
-11. HOW CAN YOU REVIEW, UPDATE OR DELETE THE DATA WE COLLECT FROM YOU? <br><br>
-      
-<h3>1. WHAT INFORMATION DO WE COLLECT?</h3>
-
+      To be precise, we collect your non-senitive user preferences, such as your choice to opt-out of bot DMs, and we save messages you submit to us with the "Report Issue" button. If that message happens to contain otherwise sensitive or personal information, take care before submitting it. There is an extra confirmation prompt to remind you before the issue actually gets submitted.
+    </p>
+    <p>
+      Other than user preferences and submitted issues, we do not collect any other data whatsoever from you, unless you are a Discord server administrator. In that case, we also store the language you have set.
+    </p>
 <p>
-None, except for your user ID, if you type sp!mute, or sp!dmmute. <br><br>
-
-<h3>2. WILL YOUR INFORMATION BE SHARED WITH ANYONE?</h3>
-
+      We do not collect any other information, period.
+    </p>
+    <h3>Why we collect it</h3>
 <p>
-No. We don't share your information, with anybody, ever. <br><br>
-
-<h3>3. IS YOUR INFORMATION TRANSFERRED INTERNATIONALLY?</h3>
-
+      Apparently, I have to explain why we collect the data that we do. Well, we collect your user and (potentially) server preferences, because that's obviously necessary for the bot to function. Otherwise, it would just ignore your opt-out preferences and send you DMs anyway. That would be a bit rude, wouldn't it?
+    </p>
+    <p>
+      We collect your submitted issues because that's the whole point of the "Report Issue" button. If we didn't collect those, then we wouldn't be able to see them, and then we wouldn't be able to address the issues. Shocker.
+    </p>
+    <h3>How it gets collected</h3>
 <p>
-No. <br><br>
-
-<h3>4. HOW LONG DO WE KEEP YOUR INFORMATION?</h3>
-
-About zero seconds in total, unless you're on a mute list, in which case we only keep your user ID. <br><br>
-
-<h3>5. HOW DO WE KEEP YOUR INFORMATION SAFE?</h3>
-
-We don't store any personal information. Our bot is open source. Our website passes security checks with flying colors. <br><br>
-
-<h3>6. WHAT ARE YOUR PRIVACY RIGHTS?</h3>
-
-You have a right to enjoy the fact that your personal information isn't collected by us. :D <br><br>
-
-<h3>7. CONTROLS FOR DO-NOT-TRACK FEATURES</h3>
-
-Why don't we have a cookie opt-out/Do not track window? Because we don't track you anyway. We don't use cookies or trackers of any kind. To remove yourself from the mute list, type sp!mute, and type sp!dmmute to remove yourself from the direct message mute list. Easy as that. <br><br>
-
-<h3>8. DO CALIFORNIA RESIDENTS HAVE SPECIFIC PRIVACY RIGHTS?</h3>
-
-We don't track you anyway, so there's nothing to opt out of. California residents do have a right to stay cool, because those beaches are sick. Come visit some time! It's fun. <br><br>
-
-<h3>9. DO WE MAKE UPDATES TO THIS NOTICE?</h3>
-
-We hope to never need to change this notice, but we might update it occasionally, typically around the time a bot update happens. <br><br>
-
-<h3>10. HOW CAN YOU CONTACT US ABOUT THIS NOTICE?</h3>
-
-If you have questions or comments about this notice, you may contact us on our Discord server. <br><br>
-
-<h3>11. HOW CAN YOU REVIEW, UPDATE, OR DELETE THE DATA WE COLLECT FROM YOU?</h3>
+      As stated above, we collect your information when you submit it to us. This is done through slash commands or buttons. We don't automatically collect sent messages or anything else.
+    </p>
+    <h3>Who your data is shared with</h3>
+    <p>
+      Well I'm supposed to list out all the thousands of third parties that most companies share their user data with, but in this case, not only do we not share any data with third parties, we don't even have any meaningful data to share. So, no one.
+    </p>
+    <p>
+      Bot developers may be granted access to the list of reported messages, so if you consider them a third party, then there you go.
+    </p>
+    <p>
+      I (the bot owner) guess technically I could go into the database and see who decided to opt-out of DMs. But I won't. I don't care.
+    </p>
+    <h3>Your rights</h3>
+    <p>
+      Based on where you live, your government may have granted you certain rights regarding your data.
+    </p>
+    <p>
+      If the GDPR applies to you, you have the right to request a copy of the data we have on you, and to request that we delete it. You can do this by contacting us at <a href="mailto:spbot@9021007.xyz">spbot@9021007.xyz</a>. If you do for some reason request a copy of your data, I'll probably just send you a screenshot of your entry in the database where I store your opt-out preference. It's not very exciting. I'll delete it, too, if you ask. If you want me to delete any reported messages, be sure to include the Message ID and Server ID. Otherwise, all I need is your User ID.
+    </p>
+    <p>
+      If you live in California, you have the right to stay cool, because those beaches are sick. Come visit some time! It's fun. You also have the right to request a copy of your data, and to request that we delete it. You can do this by contacting us at <a href="mailto:spbot@9021007.xyz">spbot@9021007.xyz</a>. If you do for some reason request a copy of your data, I'll probably just send you a screenshot of your entry in the database where I store your opt-out preference. It's not very exciting. I'll delete it, too, if you ask. If you want me to delete any reported messages, be sure to include the Message ID and Server ID. Otherwise, all I need is your User ID.
+    </p>
 <p>
-We can't. We don't collect any personal data whatsoever from you. Enjoy :)
-If you want your user ID removed from the mute databases (if you selected a preference, such as opting out of DMs), you can email us at <a href="mailto:spbot@9021007.xyz">spbot@9021007.xyz</a>.
-</p>
+      If you live somewhere else that has additional privacy rights, follow the same instructions as above for Europe and California. I'll do my best to comply with your local laws.
+    </p>
+    <h3>What if I don't want you to store my information?</h3>
+    <p>
+      Simply don't press any bot buttons or use any slash commands. By not interacting with the bot, you risk submitting any data to us. Easy as that. The bot will still function, but you won't be able to report any issues or save any preferences. 
+    </p>
+    <h3>Is my data transferred internationally?</h3>
+    <p>
+      The bot is hosted in sunny Los Angeles, California, in the United States. If you're not in the US, your data will be transferred internationally. If you're in the US, then it won't be. Welcome to Geography 101.
+    </p>
+    <p>
+      The GDPR says that I have to treat your data with the same level of protection as if it were in Europe. I'll do my best, but I can't guarantee that the US government won't come knocking on my door and demand your data. If they do, I'll let you know, unless the file a gag order or something. Then I won't be able to tell you. The point is that nobody would care about your bot opt-out preference anyway.
+    </p>
+    <h3>How long do you store my data?</h3>
+    <p>
+      Forever!!!! *dramatic sound effect*
+    </p>
+    <p>
+      Yes, I store your DM opt-out and server language preferences forever, unless you ask me to delete it. I also store your reported messages forever, unless you ask me to delete them.
+    </p>
+    <p>
+      There really shouldn't be any personal information in there anyway.
+    </p>
+    <h3>Security measures</h3>
+    <p>
+      First and foremost, I use SSL. It's 2024, everyone does that now. If you're not using SSL, you're doing it wrong.
+    </p>
+    <p>
+      Your user preferences are stored in a MySQL database, and your reported messages are stored in a Discord channel. The database stores no sensitive information, and the Discord channel is only accessible to bot developers.
+    </p>
+    <p>
+      Really, the most important security measure is that everything is open-source. You can see the code for yourself, and see that I'm not doing anything shady. If you don't know how to read code, then that's a skill issue.
+    </p>
+    <h3>Updates to this policy</h3>
+    <p>
+      We hope to never need to update this policy, but if we do, it's ususlly whenever new bot features are added, in order to account for them. Usually, I announce changes in our Discord server, so if you're in there, you'll know.
+    </p>
+    <h3>Right to complaint</h3>
+    <p>
+      You also have the right to complain to me about your privacy rights. Go ahead - <a href="mailto:spbot@9021007.xyz">spbot@9021007.xyz</a>.
+    </p>
+    <h3>Contact</h3>
+    <p>
+      Well I apparently need to reiterate that you can contact me at <a href="mailto:spbot@9021007.xyz">spbot@9021007.xyz</a>, as well as the fact that the name of the entity responsible for this privacy policy is "Suicide Prevention Bot".
+    </p>
+    <br>
+    <p>
+      Welp, that was boring. Did you make it to the end? Legally, you're supposed to. 
+    </p>
+    <p>
+      Anyway, I have fulfilled my legal requirements, making that the longest possible way to say "we store your user preferences and reported messages and nothing else."
+    </p>
 </div>
 
 <style>
+  p {
+    font-size: 1vw;
+  }
 
+  h1 {
+    font-size: 3vw;
+  }
+
+  h3 {
+    font-size: 1.8vw;
+  }
   @media screen and (orientation: landscape) {
     .policy-content {
       padding-top: calc(1rem + 70px);
